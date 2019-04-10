8ce23cd813f4e7c4d1c341d66621dab7d13f9edb
EstebanMateos <47832137+EstebanMateos@users.noreply.github.com>
2019-04-10 12:45:21 +0200

    Add files via upload

---
+++ TEST.py
@@ -0,0 +1,92 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 10 12:13:19 2019
+
+@author: 3800147
+"""
+
+Ligne = 16
+Colonne = 16
+plaine =('oifdnzoenfze',2/5)
+montagne=('ppp',1/8)
+foret=('ppp',1/3)
+environnement=montagne
+List_number_species=[20,15,10]
+age_max_esp2=4
+age_max_esp3=8
+age_max_esp4=10
+per_2 =3            #périmètre
+per_3=5
+per_4=8
+faim_e4 = 20        # à partir de combien ils ont faim
+faim_e3 =20
+faim_e2 =20
+amr_e4=12/12    #age de maturité sexuelle
+amr_e3=9/12
+amr_e2=3/12
+faim_perdue_e4=5    
+faim_perdue_e3=5
+faim_perdue_e2=5
+gestation_e4=1/9
+gestation_e3=1/6
+gestation_e2=1/3
+
+
+
+
+
+Ligne = 16
+Colonne = 16
+plaine =('oifdnzoenfze',2/5)
+montagne=('ppp',1/8)
+foret=('ppp',1/3)
+environnement=foret
+List_number_species=[22,17,11]
+age_max_esp2=3
+age_max_esp3=8
+age_max_esp4=10
+per_2 =3            #périmètre
+per_3=5
+per_4=8
+faim_e4 = 10        # à partir de combien ils ont faim
+faim_e3 =15
+faim_e2 =20
+amr_e4=12/12    #age de maturité sexuelle
+amr_e3=9/12
+amr_e2=6/12
+faim_perdue_e4=5    
+faim_perdue_e3=5
+faim_perdue_e2=5
+gestation_e4=1/9
+gestation_e3=1/6
+gestation_e2=1/3
+
+
+
+#il ne reste que des gris
+Ligne = 16
+Colonne = 16
+plaine =('oifdnzoenfze',2/5)
+montagne=('ppp',1/8)
+foret=('ppp',1/3)
+environnement=foret
+List_number_species=[22,16,11]
+age_max_esp2=3
+age_max_esp3=8
+age_max_esp4=10
+per_2 =3            #périmètre
+per_3=5
+per_4=8
+faim_e4 = 10        # à partir de combien ils ont faim
+faim_e3 =15
+faim_e2 =20
+amr_e4=12/12    #age de maturité sexuelle
+amr_e3=9/12
+amr_e2=6/12
+faim_perdue_e4=2.5
+faim_perdue_e3=5
+faim_perdue_e2=5
+gestation_e4=9/12
+gestation_e3=6/12
+gestation_e2=3/12
